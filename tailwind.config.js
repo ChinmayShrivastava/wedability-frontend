889cc490879ce68bf0b0acae3704116623709e22
Chinmay Shrivastava <79005030+ChinmayShrivastava@users.noreply.github.com>
2024-03-09 23:07:40 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,14 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1080px',
+      '2xl': '1280px',
+      '3xl': '1536px',
+    },
   },
   plugins: [],
 }
